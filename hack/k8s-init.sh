2fd167422e94ef5acdefda497aa143a1e94e2e3c
rootsongjc <rootsongjc@gmail.com>
2018-03-20 11:15:26 +0800

    uncomment the commands

--- hack/k8s-init.sh
+++ hack/k8s-init.sh
@@ -2,11 +2,13 @@
 # start the following service in node1
 if [ `hostname` == "node1" ];then
     echo "Create network config in etcd..."
+    etcdctl mkdir /kube-centos/network
+    etcdctl mk /kube-centos/network/config '{"Network":"172.33.0.0/16","SubnetLen":24,"Backend":{"Type":"host-gw"}}'
 fi
 # Start the following services in all nodes
 services=(flanneld docker kubelet)
 for svc in ${services[@]}
 do
     echo "Start $svc"
-    #sudo systemctl start $svc
+    sudo systemctl start $svc
 done
